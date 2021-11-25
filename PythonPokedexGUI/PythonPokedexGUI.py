c25120ef94350a67a5a7e32c395d2e2229d638fc
bilal asghar <asgharbilal8@gmail.com>
2021-11-25 21:11:08 +0000

    Created user interface window

--- PythonPokedexGUI/PythonPokedexGUI.py
+++ PythonPokedexGUI/PythonPokedexGUI.py
@@ -1,1 +1,11 @@
+import pypokedex
+import PIL.Image, PIL.ImageTk
+import tkinter as tk
+import urllib3 
+from io import BytesIO
 
+WINDOW =tk.Tk()
+WINDOW.geometry("600x500")
+WINDOW.title("WeSellAnyCode Python Pokedex")
+
+WINDOW.mainloop()
