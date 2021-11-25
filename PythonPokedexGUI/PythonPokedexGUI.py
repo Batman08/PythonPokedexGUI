b05fcb5ee9211e89af47debd394067454f1025ba
bilal asghar <asgharbilal8@gmail.com>
2021-11-25 21:12:46 +0000

    Added title to user interface window

--- PythonPokedexGUI/PythonPokedexGUI.py
+++ PythonPokedexGUI/PythonPokedexGUI.py
@@ -8,4 +8,8 @@ WINDOW =tk.Tk()
 WINDOW.geometry("600x500")
 WINDOW.title("WeSellAnyCode Python Pokedex")
 
+TITLE_LABEL = tk.Label(WINDOW, text="WeSellAnyCode Pokedex")
+TITLE_LABEL.config(font=("Arial", 32))
+TITLE_LABEL.pack(padx=10, pady=10)
+
 WINDOW.mainloop()
